e3b1df954ba21c2e7ce79645d5f73a6f7032e625
SLAF <SLAF@AFHQ-ITU-SOFT1>
2023-04-27 14:37:46 +0530

    wsa

---
+++ WebApplication1/Asset/js/easypiechart-data.js
@@ -0,0 +1,34 @@
+$(function() {
+    $('#easypiechart-teal').easyPieChart({
+        scaleColor: false,
+        barColor: '#1ebfae'
+    });
+});
+
+$(function() {
+    $('#easypiechart-orange').easyPieChart({
+        scaleColor: false,
+        barColor: '#ffb53e'
+    });
+});
+$(function () {
+    $('#easypiechart-green').easyPieChart({
+        scaleColor: false,
+        barColor: '#30cc7b'
+    });
+});
+
+$(function() {
+    $('#easypiechart-red').easyPieChart({
+        scaleColor: false,
+        barColor: '#f9243f'
+    });
+});
+
+$(function() {
+   $('#easypiechart-blue').easyPieChart({
+       scaleColor: false,
+       barColor: '#30a5ff'
+   });
+});
+ 
